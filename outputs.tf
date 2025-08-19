7190adcbd05665673aeb035dda1fc6b76c7c44ee
Novikov Sergey <sergey.novikov@flaconi.de>
2025-08-19 12:42:46 +0200

    OPS-6491 Fix default values for `output_options` (#9)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
 output "job" {
   description = "A created logpush job"
-  value       = { for k, v in cloudflare_logpush_job.this : k => v if k != "ownership_challenge" }
+  value = { for k, v in cloudflare_logpush_job.this :
+    k => v if !contains(["ownership_challenge", "last_complete"], k)
+  }
 }
