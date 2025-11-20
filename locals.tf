1f070202ca5dda540cb377cb042e350f90e31898
waqas.yousaf <waqas.yousaf@flaconi.de>
2025-11-20 17:12:18 +0100

    OPS-7003: lint

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-  zone_id = lookup(data.cloudflare_zones.this.result[0], "id")
-  sse_query = var.sse != null ? "&sse=${var.sse}" : ""
+  zone_id          = lookup(data.cloudflare_zones.this.result[0], "id")
+  sse_query        = var.sse != null ? "&sse=${var.sse}" : ""
   aws_region       = var.ownership_challenge == null ? data.aws_region.this[0].region : var.s3_region
   destination_conf = "s3://${var.s3_bucket_name}/${var.s3_path}?region=${local.aws_region}${local.sse_query}"
 
